6f7a4943a74c2d823ab9fa9055ae792f50e79804
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-02-22 00:15:41 +0200

    fix-bugs

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -13,3 +13,5 @@ function onMenuToggle() {
     ? (document.body.style.overflow = 'hidden')
     : (document.body.style.overflow = '');
 }
+
+//======================================================//
